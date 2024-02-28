d6358abb95f998ab0d560fe768251f9a97099a52
Matthew O <mathonich@yahoo.com>
2024-02-27 16:35:39 -0800

    Apache bench script

---
+++ ab.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check if the number of arguments is not equal to 1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 {url}"
+    exit 1
+fi
+
+url=$1
+connections=2
+concurrency=1
+
+
+# Run Apache Bench in a loop until there are failed requests
+while true; do
+    # Run Apache Bench
+    ab -n "${connections}" -c "${concurrency}" -v "3" "${url}" &> "${connections}.txt"
+
+    # Check if timed out from ab exit status
+    # Check if there are failed requests in the output
+    failed_requests=$(tail -n "25" "${connections}.txt" | head -n "1" | awk '{print $3}')
+    echo "${failed_requests} -n ${connections} -c ${concurrency}"
+
+    if [ "$failed_requests" != 0 ]; then
+        break
+    fi
+    connections=$((connections * 2))
+    concurrency=$((concurrency * 2))
+
+
+done
+
+# Display a message indicating completion
+echo "Failed ${failed_requests} at ${connections} connections and ${concurrency} concurrency"
+
